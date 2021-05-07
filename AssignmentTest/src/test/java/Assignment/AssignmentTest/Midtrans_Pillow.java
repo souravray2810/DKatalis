999bc12e27e9a822f1ac2f1bbb09f37ed3ec80bc
Sourav Ray <sourav.ray@olethia.com>
2021-05-08 00:36:59 +1000

    First Commit Test

---
+++ AssignmentTest/src/test/java/Assignment/AssignmentTest/Midtrans_Pillow.java
@@ -0,0 +1,22 @@
+package Assignment.AssignmentTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Midtrans_Pillow {
+
+	public static void main(String[] args) throws Throwable {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "/Users/rajbalani/git/DKatalis/AssignmentTest/Drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.midtrans.com/");
+		System.out.println("Welcome to Midtrans");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"container\"]/div/div/div[1]/div[2]/div/div/a")).click();
+		driver.findElement(By.className("cart-checkout")).click();
+		System.out.println("Successfully added pillow to cart");
+	}
+
+}
